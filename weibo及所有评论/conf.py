77494efc977e2d7c6ec256e52b1eb26934c2b698
wenrui2015 <1174950106@qq.com>
2018-06-07 17:22:38 +0800

    Update conf.py

--- weibo及所有评论/conf.py
+++ weibo及所有评论/conf.py
@@ -4,10 +4,12 @@ Created on Thu Jun  7 17:13:46 2018
 
 @author: ruiwen
 """
+#you should change this to you own configuration 
+PHANTOM_JS_PATH='E:/spyer_pro/crawl_data/phantomjs-2.1.1-windows/bin/phantomjs.exe'  #your phantomjs path
+COOKIES_SAVE_PATH='./cookies.pkl'   #path to save cookies
+account_id="***********"   #you weibo account for login,phone or mail in general
+account_password="******"  #you account password
+user_id="167385960"        #userid you want to scrawl,这里是微博id,比如baby的微博id为： realangelababy 或者 1642351362
 
-PHANTOM_JS_PATH='E:/spyer_pro/crawl_data/phantomjs-2.1.1-windows/bin/phantomjs.exe'
-COOKIES_SAVE_PATH='./cookies.pkl'
-account_id="18874074194"
-account_password="suibe415"
-user_id="6341745629"                                #user you want to scrawl
+#this dont need change
 url_login="https://passport.weibo.cn/signin/login"   #微博登录网址  
