e6cdcce8809da16f93d06f71c049059925f8d483
wenrui2015 <1174950106@qq.com>
2018-06-07 17:16:38 +0800

    Add files via upload

---
+++ weibo及所有评论/conf.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  7 17:13:46 2018
+
+@author: ruiwen
+"""
+
+PHANTOM_JS_PATH='E:/spyer_pro/crawl_data/phantomjs-2.1.1-windows/bin/phantomjs.exe'
+COOKIES_SAVE_PATH='./cookies.pkl'
+account_id="18874074194"
+account_password="suibe415"
+user_id="6341745629"                                #user you want to scrawl
+url_login="https://passport.weibo.cn/signin/login"   #微博登录网址  
